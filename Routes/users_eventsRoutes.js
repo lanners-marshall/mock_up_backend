988fb18226ce92c3ae00bdf2b5f41eb96eff92d8
marshall <marshall@marshalls-MacBook-Air.local>
2019-02-12 08:30:54 -0800

    favorites routes

---
+++ Routes/users_eventsRoutes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('knex')
+const dbConfig = require('../knexfile')
+const db = knex(dbConfig.development)
+
+//CREATE
+//user signs up to go to an event
+//post http://localhost:5555/users_events
+router.post('', (req, res) => {
+
+	//would have to pass this is fron the front end
+	const {user_id, event_id} = req.body
+
+	//check is user is already going to event
+	db('users_events')
+	.where({user_id, event_id})
+	.then(response => {
+
+		// if user attemps to sign up for event he is already going to
+		if (response.length > 0){
+			return res.status(200).json({msg: 'you are aleady going to this event'})
+		} else {
+			//user adds event since he is not yet going to event
+			db.insert({user_id, event_id}).into('users_events')
+			.then(response => {
+				return res.status(200).json(response)
+			})
+			//catch error for adding event
+			.catch(error => {
+				return res.status(500).json(error)
+			})
+		}
+	})
+	//catch error for looking up if user is going to event
+	.catch(error => {
+		return res.status(500).json(error)
+	})
+})
+
+
+//DELETE
+//delete user no longer going to event
+//delete http://localhost:5555/users_events
+//-------------------------------------------
+router.delete('', (req, res) => {
+	const {user_id, event_id} = req.body
+	db('users_events')
+	.where({user_id, event_id})
+	.del()
+	.then(response => {
+		console.log(response)
+		return res.status(200).json(response)
+	})
+	.catch(error => {
+		return res.status(200).json(error)
+	})
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -17,13 +17,13 @@ server.use(cors())
 const userRoutes = require('./Routes/usersRoutes')
 const eventsRoutes = require('./Routes/eventsRoutes')
 const users_eventsRoutes = require('./Routes/users_eventsRoutes')
-// const favoritesRoutes = require('./Routes/favoritesRoutes')
+const favoritesRoutes = require('./Routes/favoritesRoutes')
 // const friendsRoutes = require('./Routes/friendsRoutes')
 
 server.use('/users', userRoutes)
 server.use('/events', eventsRoutes)
 server.use('/users_events', users_eventsRoutes)
-// server.use('/favorites', favoritesRoutes)
+server.use('/favorites', favoritesRoutes)
 // server.use('/friends', friendsRoutes)
 
 let app = server.listen(process.env.PORT || 5555, function () {
